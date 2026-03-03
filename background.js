d1bb581fe2ebb63ac98e87ee514004a1308f4391
hemakoppireddy <hemakoppireddy28@gmail.com>
2026-03-03 19:37:18 +0530

    Add session saving functionality; implement keyboard shortcut to save current tabs as a session

--- background.js
+++ background.js
@@ -21,3 +21,27 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Ignore invalid URLs like chrome://
   }
 });
+
+// Keyboard shortcut handler
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "save-session") {
+
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+      const urls = tabs.map(tab => tab.url);
+
+      const sessionName = "quick-save-" + Date.now();
+
+      chrome.storage.local.get(["sessions"], (data) => {
+        const existingSessions = data.sessions || {};
+
+        const updatedSessions = {
+          ...existingSessions,
+          [sessionName]: urls
+        };
+
+        chrome.storage.local.set({ sessions: updatedSessions });
+      });
+    });
+
+  }
+});
